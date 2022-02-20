d9088b747fc19478ee1a141bd852e2b68a993580
jonathanhoy <jonathanhoy5@gmail.com>
2022-02-19 22:02:19 -0500

    separated and refactored tool nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { NavLink } from 'react-router-dom';
 import variables from '../styles/variables';
 import { Wrapper } from '../styles/Wrapper';
+import ToolNav from "./ToolNav";
 
 class Header extends React.Component {
 
@@ -50,18 +51,7 @@ class Header extends React.Component {
               <i className="fas fa-times fa-fw"></i>
           </button>
         </Wrapper>
-        <StyledNav mobileNavHidden={this.state.mobileNavHidden} >
-          <Wrapper>
-            <ul className="tool-nav">
-              <li><NavLink onClick={this.handleClose} to="/calculator" exact activeClassName="active"><i className="fas fa-calculator"></i> Calculator</NavLink></li>
-              <li><NavLink onClick={this.handleClose} to="/clock" exact activeClassName="active"><i className="fas fa-clock"></i> Clock</NavLink></li>
-            </ul>
-            <ul className="site-nav">
-              <li><NavLink onClick={this.handleClose} to="/resources" exact activeClassName="active">Resources</NavLink></li>
-              <li><NavLink onClick={this.handleClose} to="/about" exact activeClassName="active">About</NavLink></li>
-            </ul>
-          </Wrapper>
-        </StyledNav>
+        <ToolNav />
       </StyledHeader>
     )
   }
@@ -147,103 +137,8 @@ const StyledHeader = styled.header`
     }
     .top-nav {
       .desktop-top-nav {
-        display: none;
+        display: none;  
       }
-    }
-  }
-`;
-
-const StyledNav = styled.nav`
-  background: ${variables.grey100};
-  > div {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-top: 0;
-  }
-  ul {
-    display: flex;
-    justify-content: center;
-    margin: 0;
-    padding: 0.5rem 0;
-    li {
-      a {
-        display: inline-block;
-        text-decoration: none;
-        color: ${variables.black};
-        font-size: 1rem;
-        font-weight: 600;
-      }
-    }
-  }
-
-  .tool-nav {
-    li {
-      margin-right: 1rem;
-      a {
-        background-color: ${variables.grey400};
-        padding: 0.5rem 1rem;
-        border-radius: 0.25rem;
-        i {
-          margin-right: 0.5rem;
-        }
-        &.active {
-          background-color: ${variables.primary900};
-          color: ${variables.white};
-        }
-      }
-    }
-    @media (max-width: ${variables.sm}) {
-      li {
-        text-align: right;
-        margin-right: 0;
-      }
-    }
-  }
-
-  .site-nav {
-    display: none;
-    @media (max-width: ${variables.sm}) {
-      display: block;
-    }
-  }
-
-  @media (max-width: ${variables.sm}) {
-    box-shadow: ${variables.boxshadow};
-    display: ${props => props.mobileNavHidden === true ? 'none' : 'block'};
-    position: absolute;
-    z-index: 20;
-    left: 0;
-    right: 0;
-    > div {
-      flex-direction: column;
-      align-items: flex-end;
-      padding: 1rem 0;
-    }
-    ul {
-      flex-direction: column;
-      margin: 0;
-      padding: 0;
-      li {
-        margin-bottom: 1rem;
-        margin-right: 0;
-        a {
-          font-size: 1.5rem;
-          display: inline-block;
-        }
-      }
-      &.site-nav {
-        text-align: right;
-      }
-      .tool-nav, .site-nav {
-      margin: 0;
-      li {
-        margin-bottom: 1rem;
-        a {
-          display: inline-block;
-        }
-      }
-    }
     }
   }
 `;
