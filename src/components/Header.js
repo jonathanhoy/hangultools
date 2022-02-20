1ff5c40bfa458e6ccef14046c40b5b90b39af834
jonathanhoy <jonathanhoy5@gmail.com>
2022-02-19 22:25:32 -0500

    refactored header for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,17 +39,19 @@ class Header extends React.Component {
     return (
       <StyledHeader mobileNavHidden={this.state.mobileNavHidden} >
         <Wrapper>
-          <div className="top-nav">
+          <div className="container">
             <h1><NavLink onClick={this.handleClose} to="/" exact activeClassName="active">Hangul Tools</NavLink></h1>
-            <ul className="desktop-top-nav">
-              <li><NavLink onClick={this.handleClose} to="/resources" exact activeClassName="active">Resources</NavLink></li>
-              <li><NavLink onClick={this.handleClose} to="/about" exact activeClassName="active">About</NavLink></li>
-            </ul>
+            <nav>
+              <ul>
+                <li><NavLink onClick={this.handleClose} to="/resources" exact activeClassName="active">Resources</NavLink></li>
+                <li><NavLink onClick={this.handleClose} to="/about" exact activeClassName="active">About</NavLink></li>
+              </ul>
+            </nav>
+            <button onClick={this.handleClick}>
+                <i className="fas fa-bars fa-fw"></i>
+                <i className="fas fa-times fa-fw"></i>
+            </button>
           </div>
-          <button className="mobile-nav-toggle" onClick={this.handleClick}>
-              <i className="fas fa-bars fa-fw"></i>
-              <i className="fas fa-times fa-fw"></i>
-          </button>
         </Wrapper>
         <ToolNav />
       </StyledHeader>
@@ -67,77 +69,101 @@ const StyledHeader = styled.header`
   > div {
     padding-top: 0;
   }
-  h1 {
-    margin: 0;
-    padding: 1rem 0;
-    font-size: 2.5rem;
-    color: ${variables.white};
-    font-weight: 600;
-    a {
-      text-decoration: none;
-      color: ${variables.white};
-    }
-  }
-  .mobile-nav-toggle {
-    display: none;
-  }
-  .top-nav {
+  .container {
+    padding-top: 0;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    .desktop-top-nav {
-      display: flex;
-      li {
-        margin-right: 1.5rem;
-        &:last-of-type {
-          margin-right: 0;
-        }
-        a {
-          text-decoration: none;
-          color: ${variables.white};
-          font-size: 1.25rem;
-          font-weight: 600;
-          &:hover {
-            color: ${variables.primary100};
+
+    h1 {
+      margin: 0;
+      padding: 1rem 0;
+      font-size: 2.5rem;
+      color: ${variables.white};
+      font-weight: 600;
+      a {
+        text-decoration: none;
+        color: ${variables.white};
+      }
+    }
+
+    nav {
+      ul {
+        display: flex;
+        li {
+          margin-right: 1.5rem;
+          &:last-of-type {
+            margin-right: 0;
+          }
+          a {
+            text-decoration: none;
+            color: ${variables.white};
+            font-size: 1.25rem;
+            font-weight: 600;
+            &:hover {
+              color: ${variables.primary100};
+            }
           }
         }
       }
     }
+
+    button {
+      display: none;
+    }
   }
+
+
   @media (max-width: ${variables.sm}) {
-    position: fixed;
-    .mobile-nav-toggle {
-      display: block;
-      position: relative;
-      background: none;
-      border: none;
-      padding: 0;
-      font-size: 1.5rem;
-      i {
-        color: ${variables.white};
+    .container {
+      h1 {
+        position: relative;
+        z-index: 30;
+        padding: 0.5rem 0;
       }
-      .fa-bars {
-        display: ${props => props.mobileNavHidden ? 'block' : 'none'};
+
+      nav {
+        display: ${props => props.mobileNavHidden === true ? 'none' : 'block'};
+        position: absolute;
+        background: ${variables.grey100};
+        width: 100%;
+        left: 0;
+        z-index: 50;
+        top: 63px;
+        padding: 1.5rem 0;
+        border-bottom: 1px solid ${variables.grey400};
+
+        ul {
+          width: 95%;
+          margin: 0 auto;
+          flex-direction: column;
+          align-items: flex-end;
+
+          li {
+            margin: 1rem 0;
+            a {
+              color: ${variables.black};
+            }
+          }
+        }
       }
-      .fa-times {
-        display: ${props => props.mobileNavHidden ? 'none' : 'block'};
-      }
-    }
-    > div {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-    h1 {
-      text-align: left;
-      position: relative;
-      z-index: 30;
-      padding: 0.5rem 0;
-      color: ${variables.white};
-    }
-    .top-nav {
-      .desktop-top-nav {
-        display: none;  
+      
+      button {
+        display: block;
+        position: relative;
+        background: none;
+        border: none;
+        padding: 0;
+        font-size: 1.5rem;
+        i {
+          color: ${variables.white};
+        }
+        .fa-bars {
+          display: ${props => props.mobileNavHidden ? 'block' : 'none'};
+        }
+        .fa-times {
+          display: ${props => props.mobileNavHidden ? 'none' : 'block'};
+        }
       }
     }
   }
