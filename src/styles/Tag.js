ddd59dcff7f7d29a184b23393d3e559893490220
jonathanhoy <jonathanhoy5@gmail.com>
2022-03-03 15:25:26 -0500

    tag style fix

--- src/styles/Tag.js
+++ src/styles/Tag.js
@@ -11,15 +11,12 @@ const Tag = styled.p`
   font-weight: 500;
   font-size: 0.875rem;
   margin-right: 0.25rem;
-  margin-bottom: 0;
+  margin-bottom: 0.25rem;
   margin-top: 0;
   @media (max-width: ${variables.sm}) {
     margin-right: 0;
     width: ${props => props.home ? '100%' : 'auto'};
     text-align: center;
-    &:not(:last-of-type) {
-      margin-bottom: 0.25rem;
-    }
   }
 `;
 
