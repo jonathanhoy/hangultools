4f38934353e029b4ae06093dfd57a7c37ae6077d
Jonathan Hoy <36137489+jonathanhoy@users.noreply.github.com>
2023-11-07 17:34:02 -0500

    code clean-up

--- src/components/resources/home.js
+++ src/components/resources/home.js
@@ -3,9 +3,6 @@ import { PageSubHeading } from '../../styles/PageHeading';
 import { 
   ResourcesSection
 } from '../../styles/Resources';
-import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
-import variables from '../../styles/variables';
 
 const ResourceHome = () => (
   <ResourcesSection id="home">
